aa0239367e9359b48778a006d6a804ce1a846564
thore <thore@MSI.localdomain>
2020-09-20 20:33:30 +0200

    seleniumTest

--- CarLookLtd.clone/src/test/java/LoginTest.java
+++ CarLookLtd.clone/src/test/java/LoginTest.java
@@ -1,44 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-/**
- *
- * @author thore
- */
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import java.util.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 public class LoginTest {
-    
-    @Test
-    public void testSimple() throws Exception {
-        // Create a new instance of the Firefox driver
-        // Notice that the remainder of the code relies on the interface, 
-        // not the implementation.
-        WebDriver driver = new FirefoxDriver();
-
-        // And now use this to visit NetBeans
-        driver.get("http://www.netbeans.org");
-        // Alternatively the same thing can be done like this
-        // driver.navigate().to("http://www.netbeans.org");
-
-        // Check the title of the page
-        // Wait for the page to load, timeout after 10 seconds
-        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-            @Override
-            public Boolean apply(WebDriver d) {
-                return d.getTitle().contains("NetBeans");
-            }
-        });
-
-        //Close the browser
-        driver.quit();
-    }
-    
+  private WebDriver driver;
+  private Map<String, Object> vars;
+  JavascriptExecutor js;
+  @Before
+  public void setUp() {
+    driver = new FirefoxDriver();
+    js = (JavascriptExecutor) driver;
+    vars = new HashMap<String, Object>();
+  }
+  @After
+  public void tearDown() {
+    driver.quit();
+  }
+  @Test
+  public void logintest() {
+    driver.get("http://localhost:8080/CarLookLtd.clone");
+    driver.manage().window().setSize(new Dimension(550, 691));
+    driver.findElement(By.cssSelector(".v-slot:nth-child(1) > .v-button")).click();
+    driver.findElement(By.id("gwt-uid-3")).click();
+    driver.findElement(By.id("gwt-uid-3")).sendKeys("test@carlook.de");
+    driver.findElement(By.id("gwt-uid-5")).sendKeys("test");
+    driver.findElement(By.cssSelector(".v-button")).click();
+  }
 }
